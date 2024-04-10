927443fbd36d6fac6b742881f3cc0b846eb551f8
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2024-04-11 02:00:15 +0300

    Deneyim ekleme ve güncelleme yapıldı

--- MyPortfolio/ViewComponents/_ExprienceComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ExprienceComponentPartial.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ExprienceComponentPartial : ViewComponent
     {
+        MyPoftfolioContext context = new MyPoftfolioContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
     }
 }

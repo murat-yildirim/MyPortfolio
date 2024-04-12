65d05d86b1d120eb6e9caae113e0ee08359f311d
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2024-04-12 19:01:29 +0300

    final

--- MyPortfolio/DAL/Context/MyPoftfolioContext.cs
+++ MyPortfolio/DAL/Context/MyPoftfolioContext.cs
@@ -19,6 +19,7 @@ namespace MyPortfolio.DAL.Context
         public DbSet<Skill> Skills { get; set; }
         public DbSet<SocialMedia> SocialMedias { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
 
     }
 }
--- MyPortfolio/Migrations/20240412151548_mig2.Designer.cs
+++ MyPortfolio/Migrations/20240412151548_mig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPortfolio.DAL.Context;
 
@@ -11,9 +12,10 @@ using MyPortfolio.DAL.Context;
 namespace MyPortfolio.Migrations
 {
     [DbContext(typeof(MyPoftfolioContext))]
-    partial class MyPoftfolioContextModelSnapshot : ModelSnapshot
+    [Migration("20240412151548_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

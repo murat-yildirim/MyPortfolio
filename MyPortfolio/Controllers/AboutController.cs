5ca351f865acc215fa72accd62b8e19dc340cfba
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2024-08-19 00:06:52 +0300

    Admin tarafında hakkımda kısmanda güncelleme işlemi yapıldı , yetenekler kısmında listeleme işlemi yapıldı

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyPoftfolioContext context = new MyPoftfolioContext();
+
+		[HttpGet]
+		public IActionResult AboutUpdate()
+        {
+			var value = context.Abouts.Find(1);
+			return View(value);
+		}
+		[HttpPost]
+		public IActionResult AboutUpdate(About about)
+		{
+
+			context.Abouts.Update(about);
+			context.SaveChanges();
+			return RedirectToAction("AboutUpdate");
+
+		}
+	}
+}
---
+++ MyPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class SkillController : Controller
+	{
+		MyPoftfolioContext context = new MyPoftfolioContext();
+		public IActionResult SkillList()
+		{
+			var values = context.Skills.ToList();
+			return View(values);
+		}
+	}
+}
